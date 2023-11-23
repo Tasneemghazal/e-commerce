ae4bdea8ba518153634c0edd1447a7a0b6a3306f
Tasneem <tasneemgazal5@gmail.com>
2023-11-23 19:50:50 +0200

    import react-toastify

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <ToastContainer />
     <App />
- 
-)
+  </>
+);
