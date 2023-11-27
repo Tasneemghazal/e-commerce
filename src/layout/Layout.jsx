88ecb30df73c57425bc247a63808392585c660bd
Tasneem <tasneemgazal5@gmail.com>
2023-11-27 15:49:54 +0200

    add setUser as parameter to Layout function

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Navbar from '../components/web/navbar/Navbar.jsx'
 import Footer from '../components/web/footer/Footer.jsx'
 import {Outlet} from 'react-router-dom'
-export default function Layout({user}) {
+export default function Layout({user,setUser}) {
   return (
     <>
-    <Navbar user={user}/>
+    <Navbar user={user} setUser={setUser}/>
     <Outlet/>
     <Footer/>
     </>
