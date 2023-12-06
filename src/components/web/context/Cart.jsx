507f14d33a8d4cf129c17003ab2bbdf106fce606
Tasneem <tasneemgazal5@gmail.com>
2023-12-06 06:14:47 +0200

    Create cart Context component

---
+++ src/components/web/context/Cart.jsx
@@ -0,0 +1,47 @@
+import { createContext, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+export const CartContext = createContext(null);
+export function CartContextProvider({ children }) {
+const addToCartContext=async(productId) => {
+    try{
+        const token = localStorage.getItem("userToken");
+        const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/cart`,
+        {productId},{headers:{Authorization:`Tariq__${token}`}}
+        );
+        if (data.message == "success") {
+          toast.success("product added successfully", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+         
+        }
+        return data;
+    }
+    catch(err){
+        console.log(err);
+    }
+};
+const getCartContext = async()=>{
+  try{
+    const token = localStorage.getItem("userToken");
+    const {data} = await axios.get(`${import.meta.env.VITE_API_URL}/cart`,{headers:{Authorization:`Tariq__${token}`}});
+    return data;
+  }
+  catch(err){
+    console.log(err);
+  }
+};
+
+  return (
+    <CartContext.Provider value={{addToCartContext,getCartContext}}>
+      {children}
+    </CartContext.Provider>
+  );
+}
