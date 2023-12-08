9274091e3c17f05e8d5aa212e81f2e8143bfb54c
Tasneem <tasneemgazal5@gmail.com>
2023-12-08 15:08:52 +0200

    add userContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,15 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { UserContextProvider } from "./components/web/context/User.jsx";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <ToastContainer />
-    <App />
-  </QueryClientProvider>
-  
+  <UserContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ToastContainer />
+      <App />
+    </QueryClientProvider>
+  </UserContextProvider>
 );
