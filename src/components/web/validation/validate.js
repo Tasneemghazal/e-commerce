36aa00f9abc1170de23082a42afff3159dc5b741
Tasneem <tasneemgazal5@gmail.com>
2023-12-08 19:56:32 +0200

    add forget password validation

--- src/components/web/validation/validate.js
+++ src/components/web/validation/validate.js
@@ -20,3 +20,12 @@ export const loginSchema = yup.object({
     .min(6, "password must be at least 6 characters")
     .max(16, "password musn't exceed 16 characters"),
 });
+export const forgetPassword = yup.object({
+  email: yup.string().email().required("email is Required "),
+  password: yup
+    .string()
+    .required("password is Required ")
+    .min(6, "password must be at least 6 characters")
+    .max(16, "password musn't exceed 16 characters"),
+  code: yup.string().required("code is Required ").min(4, "code must be 4 characters").max(4,"code must be 4 characters")
+});
